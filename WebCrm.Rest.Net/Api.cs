e565bfd7b81f04c3d6f7afa027e4dd25d6771e8d
Ole Jørgensen <olejorgensen@me.com>
2019-10-23 09:26:51 +0200

    UpdateLogin is called from GET<T>

--- WebCrm.Rest.Net/Api.cs
+++ WebCrm.Rest.Net/Api.cs
@@ -14,14 +14,11 @@ namespace WebCrm.Rest.Net
         /// <returns>Modtag alle OrganisationDto</returns>
         public IEnumerable<OrganisationDto> Organisations()
         {
-            Client.UpdateLogin();
             return Client.GET<OrganisationDto>("Organisations");
         }
 
         public IEnumerable<OrganisationDto> OrganisationWithDuplicateVats()
         {
-            Client.UpdateLogin();
-
             var dupes = Organisations()
                 .GroupBy(o => o.OrganisationVatNumber)
                 .Where(g => g.ToList().Count() > 1)
@@ -34,35 +31,30 @@ namespace WebCrm.Rest.Net
         /// <returns>Modtag en OrganisationDto med det angivne ID</returns>
         public OrganisationDto Organisation(int id)
         {
-            Client.UpdateLogin();
             return Client.GET<OrganisationDto>("Organisations", id);
         }
 
         /// <returns>Modtag alle PersonDto</returns>
         public IEnumerable<PersonDto> Persons()
         {
-            Client.UpdateLogin();
             return Client.GET<PersonDto>("Persons");
         }
 
         /// <returns>Modtag en PersonDto med det angivne ID</returns>
         public PersonDto Person(int id)
         {
-            Client.UpdateLogin();
             return Client.GET<PersonDto>("Persons", id);
         }
 
         /// <returns>Modtag alle RelationDto'er</returns>
         public IEnumerable<RelationDto> Relations()
         {
-            Client.UpdateLogin();
             return Client.GET<RelationDto>("Relations");
         }
 
         /// <returns>Modtag en RelationDTO med det angivne ID</returns>
         public RelationDto Relation(int id)
         {
-            Client.UpdateLogin();
             return Client.GET<RelationDto>("Relations", id);
         }
 
